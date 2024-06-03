5883a62a016a95126773c21fd585240c879ff119
jceferino-2022238 <jceferino-2022238@kinal.edu.gt>
2024-06-03 10:06:01 -0600

    Help info

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,9 +1,10 @@
 import { DashboardPage } from "./pages/dashboard/DashboardPage";
 import { AuthPage } from "./pages/auth";
-
+import { TestDashboard } from "./pages/dashboard/TestDashboard";
 const routes = [
     {path: '/*', element:<AuthPage/>},
-    {path: '/dashboard', element: <DashboardPage/>}
+    {path: '/dashboard', element: <DashboardPage/>},
+    {path:'/dashboardTest', element: <TestDashboard/>}
 ]
 
 export default routes
