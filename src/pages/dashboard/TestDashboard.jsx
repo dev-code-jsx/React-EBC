d2e028f44bf9654de84613ceaaebb4f0aed3b05c
jceferino-2022238 <jceferino-2022238@kinal.edu.gt>
2024-06-03 17:19:14 -0600

    Transfer Form done

--- src/pages/dashboard/TestDashboard.jsx
+++ src/pages/dashboard/TestDashboard.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { HelpInfo } from '../../components/help/HelpInfo'
+import { TransferTable } from '../../components/transferForm/TransferTable'
+import { TransferMForm } from '../../components/transferForm/TransferMForm'
 export const TestDashboard = () => {
   return (
     <div>
-      <HelpInfo/>
+      <TransferMForm/>
     </div>
   )
 }
